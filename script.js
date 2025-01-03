f7f5352652c57578cefa3cd011aa0a0661a4b853
OsteroyergoyJA <oosteroyergoy@gmail.com>
2025-01-03 14:18:49 +0100

    Lagt til addEventListener

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Hello!');
+let myGames = [' Age of Empires', ' Hogwarts Legacy', ' The Witcher',]
+
+const topListButton = document.querySelector('#button4');
+
+const newArray = document.createElement('h3');
+
+newArray.innerText = myGames;
+
+document.querySelector('main').appendChild(newArray);
+
+topListButton.addEventListener('click', function(){
+
+})
+
+
